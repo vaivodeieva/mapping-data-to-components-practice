4e9e14884fc9728c7306b9a4e12a64ff19a6f512
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-06-29 15:44:13 +0300

    edit files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from "./components/App";
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,4 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
 
